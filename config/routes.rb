46f29bf913391932195d3309824fbfb7f7288262
beNjiox <benjamin@guez.fr>
2014-05-20 12:48:21 +0200

    Added Api Constraints.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
+require 'api_constraints'
+
 Ror::Application.routes.draw do
-  scope defaults: { format: :json } do
+  scope defaults: { format: :json }, constraints: ApiConstraints.new(version: 1, default: true) do
     resources :lists do
       resources :contacts
     end
